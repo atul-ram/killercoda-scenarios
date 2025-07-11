24b7e670ed503f7a513de5f3dbe9285737d3f28a
atul sahu <atul.ram@gmail.com>
2025-07-11 05:18:52 +0200

    a

--- prometheus/foreground.sh
+++ prometheus/foreground.sh
@@ -2,19 +2,19 @@ echo "Deploying scenario..." && sleep 30
 
 kubectl get pod -n default
 
-kubectl wait --for=condition=ready --timeout=30s pod --all -n default
+kubectl wait --for=condition=ready --timeout=150s pod --all -n default
 
 kubectl get pod -n default
 
 
-kubectl wait --for=condition=ready --timeout=30s pod --all -n default
+kubectl wait --for=condition=ready --timeout=50s pod --all -n default
 
-kubectl wait --for=condition=ready --timeout=30s pod --all -n default
+#kubectl wait --for=condition=ready --timeout=30s pod --all -n default
 sleep 5
+#kubectl wait --for=condition=ready --timeout=30s pod --all -n default
+#sleep 5
 kubectl wait --for=condition=ready --timeout=30s pod --all -n default
-sleep 5
-kubectl wait --for=condition=ready --timeout=30s pod --all -n default
-kubectl wait --for=condition=ready --timeout=30s pod --all -n default
+#kubectl wait --for=condition=ready --timeout=30s pod --all -n default
 
 kubectl get pod -n default
 
