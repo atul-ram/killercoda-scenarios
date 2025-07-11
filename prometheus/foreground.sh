72a05666a22b4618ed363bf1e410ab79b29fdf66
atul sahu <atul.ram@gmail.com>
2025-07-11 05:01:21 +0200

    get

--- prometheus/foreground.sh
+++ prometheus/foreground.sh
@@ -1,3 +1,3 @@
 echo "Deploying scenario..." && sleep 10
 
-kubectl wait --for=condition=ready --timeout=3000s pod -l app.kubernetes.io/name=grafana -n default
+kubectl wait --for=condition=ready --timeout=3000s get pod -l app.kubernetes.io/name=grafana -n default
