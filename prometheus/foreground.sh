76587c860b0ba00d6f0a2828b8fec5ae3e5bfbb0
atul sahu <atul.ram@gmail.com>
2025-07-11 05:12:07 +0200

    z

--- prometheus/foreground.sh
+++ prometheus/foreground.sh
@@ -1,3 +1,10 @@
 echo "Deploying scenario..." && sleep 30
 
+kubectl get pod -n default
+
+kubectl wait --for=condition=ready --timeout=300s pod --all -n default
+
+kubectl get pod -n default
+
+
 kubectl wait --for=condition=ready --timeout=300s pod --all -n default
