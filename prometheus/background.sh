3c9189df02714430d765c265497400ca224c3146
atul sahu <atul.ram@gmail.com>
2025-07-11 04:21:51 +0200

    chk prometheus stack

--- prometheus/background.sh
+++ prometheus/background.sh
@@ -2,7 +2,13 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 
 helm repo update
 
-helm install prometheus prometheus-community/prometheus -f https://raw.githubusercontent.com/chadmcrowell/k8s/main/prometheus/values.yaml
+helm install prometheus prometheus-community/kube-prometheus-stack \
+  --set prometheus.prometheusSpec.serviceMonitorSelectorNilUsesHelmValues=false \
+  --set prometheus.prometheusSpec.serviceMonitorSelector.matchExpressions[0].key=k8s-app \
+  --set prometheus.prometheusSpec.serviceMonitorSelector.matchExpressions[0].operator=Exists \
+  --set prometheus.prometheusSpec.serviceMonitorSelector.matchExpressions[0].values[0]=kube-prometheus-stack-prometheus-node-exporter
+
+#prometheus -f https://raw.githubusercontent.com/chadmcrowell/k8s/main/prometheus/values.yaml
 
 # helm install node-exporter prometheus-community/prometheus-node-exporter
 # helm install node-exporter prometheus-community/prometheus-node-exporter --set service.targetPort=9101 --set service.port=9101
