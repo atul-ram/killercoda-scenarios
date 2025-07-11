14c86aa1d423623d2b5bd1d1830ff04746fb676d
atul-ram <atul.ram@gmail.com>
2025-07-11 09:16:34 +0200

    Update foreground.sh

--- prometheus/foreground.sh
+++ prometheus/foreground.sh
@@ -2,12 +2,12 @@ echo "Deploying scenario..." && sleep 50
 
 kubectl get pod -n default
 
-kubectl wait --for=condition=ready  pod --all -n default
+#kubectl wait --for=condition=ready  pod --all -n default
 
 kubectl get svc -n default
 
 echo "Scenario deployed successfully."
 
-kubectl patch svc prometheus-grafana -p '{"spec": {"type": "NodePort", "ports": [{"port": 80, "nodePort": 30000}]}}'
+#kubectl patch svc prometheus-grafana -p '{"spec": {"type": "NodePort", "ports": [{"port": 80, "nodePort": 30000}]}}'
 
-echo "Grafana service patched to NodePort on port 30000."
+#echo "Grafana service patched to NodePort on port 30000."
