d4946f97e935aa7cfb7f124741e4574bfe49eab7
atul sahu <atul.ram@gmail.com>
2025-07-11 06:12:55 +0200

    z

--- prometheus/foreground.sh
+++ prometheus/foreground.sh
@@ -19,7 +19,3 @@ kubectl wait --for=condition=ready --timeout=30s pod --all -n default
 kubectl get pod -n default
 
 echo "Scenario deployed successfully."
-
-echo "You can port-forward the Prometheus server to access it locally:"
-echo "kubectl port-forward svc/prometheus-server 9090:80 -n default"
-echo "Then visit http://localhost:9090 in your web browser."
